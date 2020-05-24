5da1e670f4057e98685a5e4f65bafed5e7919fb5
ericfs <ericfs>
2020-05-24 18:36:27 -0400

    add additional keypad support for thirds

--- shortcuts/thirds.js
+++ shortcuts/thirds.js
@@ -4,7 +4,10 @@
 const thirds = [
   [1, HYPER, [WindowPosition.THIRD_LEFT]],
   [2, HYPER, [WindowPosition.THIRD_MIDDLE]],
-  [3, HYPER, [WindowPosition.THIRD_RIGHT]]
+  [3, HYPER, [WindowPosition.THIRD_RIGHT]],
+  ['keypad1', HYPER, [WindowPosition.THIRD_LEFT]],
+  ['keypad2', HYPER, [WindowPosition.THIRD_MIDDLE]],
+  ['keypad3', HYPER, [WindowPosition.THIRD_RIGHT]]
 ];
 
 setKeysHandler ( setFrame, thirds );
