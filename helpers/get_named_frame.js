627831254ab177d92cc1a198a9acc5cc0dc3492b
ericfs <ericfs>
2020-10-23 09:24:26 -0400

    change from 60/40 split to thirds.

--- helpers/get_named_frame.js
+++ helpers/get_named_frame.js
@@ -9,12 +9,12 @@ g.WindowPosition = {
     TOP: {x:0, y:0, width:1, height:.5 + dTop},
     RIGHT: {x:.5 + dLeft, y:0, width:.5 - dLeft, height:1},
     BOTTOM: {x:0, y:.5 + dTop, width:1, height:.5 - dTop},
-    LEFT: {x:0, y:0, width:.5 + dLeft, height:1},
+    LEFT: {x:0, y:0, width:.5 - dLeft, height:1},
     /* CORNERS */
-    TOP_LEFT: {x:0, y:0, width:.5 + dLeft, height:.5 + dTop},
+    TOP_LEFT: {x:0, y:0, width:.5 - dLeft, height:.5 + dTop},
     TOP_RIGHT: {x:.5 + dLeft, y:0, width:.5 - dLeft, height:.5 + dTop},
     BOTTOM_RIGHT: {x:.5 + dLeft, y:.5 + dTop, width:.5 - dLeft, height:.5 - dTop},
-    BOTTOM_LEFT: {x:0, y:.5 + dTop, width:.5 + dLeft, height:.5 - dTop},
+    BOTTOM_LEFT: {x:0, y:.5 + dTop, width:.5 - dLeft, height:.5 - dTop},
     /* HALVES */
     HALF_LEFT: {x:0/2, y:0, width:1/2, height:1},
     HALF_RIGHT: {x:1/2, y:0, width:1/2, height:1},
